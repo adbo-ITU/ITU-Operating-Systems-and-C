f380892274f536d08a26f38306ea8ad28a73d683
Adrian Borup <adrian@borup.com>
2022-08-30 18:50:04 +0000

    Add initial lecture 1 exercise solutions

---
+++ lecture-1/ex3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  // If not initialized, the following is printed:
+  //   I am -1405059071 years old.
+  int age = 10;
+  int height = 72;
+
+  char *lulw = NULL;
+  printf("%c", lulw[123]);
+
+  // If age is removed, the following is printed:
+  //   I am 67600152 years old.
+  printf("I am %d years old.\n", age);
+  printf("I am %d inches tall.\n", height);
+
+  return 0;
+}
