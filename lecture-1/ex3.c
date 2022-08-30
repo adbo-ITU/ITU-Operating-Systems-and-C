7c13d70b4472eb701073d8f3009571160150be2b
Adrian Borup <adrian@borup.com>
2022-08-30 19:01:13 +0000

    Comment out code that segfaults ex3

--- lecture-1/ex3.c
+++ lecture-1/ex3.c
@@ -7,8 +7,9 @@ int main(int argc, char *argv[])
   int age = 10;
   int height = 72;
 
-  char *lulw = NULL;
-  printf("%c", lulw[123]);
+  // To crash the system and activate the debugger, uncomment the following:
+  // char *lulw = NULL;
+  // printf("%c", lulw[123]);
 
   // If age is removed, the following is printed:
   //   I am 67600152 years old.
