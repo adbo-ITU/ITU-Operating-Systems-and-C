28a0a85e31e447e6fe407dbd916f0c5e38eb88bd
Adrian Borup <adrian@borup.com>
2022-11-20 10:11:48 +0000

    Move around functions

--- 22-Lab3-malloclab/mm.c
+++ 22-Lab3-malloclab/mm.c
@@ -68,114 +68,33 @@ team_t team = {
 
 #define max(x, y) ((x) > (y) ? (x) : (y))
 
-static void *heap_listp = NULL;
+/* Private functions in this file */
+static void *coalesce(void *bp);
+static void *find_fit(size_t size);
+static void *extend_heap(size_t words);
+static void place(void *ptr, size_t size);
+static void heapdump();
 
-void print_block(void *ptr) {
-    unsigned int* next = (unsigned int*) get_next_block_ptr(ptr);
-    printf("[%s] Addr: %p. Size: %d. Next: %p.\n",
-      is_allocated(get_header_ptr(ptr)) ? "Allocated" : "Free     ",
-      ptr, get_size(get_header_ptr(ptr)), next);
-}
-
-void heapdump() {
-  printf("\n");
-  printf("----------------- HEAP DUMP -----------------\n");
-
-  printf("- Heap boundary: %p to %p\n", mem_heap_lo(), mem_heap_hi());
-  printf("- Heap size: %d bytes\n", mem_heapsize());
-  printf("- Heap start ptr: %p\n", heap_listp);
-
-  printf("\nHeap dump:\n");
-
-  // Print all words
-  // int n = 0;
-  // unsigned int* p = (unsigned int*) mem_heap_lo();
-  // while ((void *) p <= (unsigned int*) mem_heap_hi() +  1) {
-  //   printf("[%4d]  %p: 0x%x\n", n, p, *p);
-  //   p += 1;
-  //   n += 1;
-  // }
-
-  // Print all blocks
-  int n = 0;
-  unsigned int* ptr = (unsigned int*) heap_listp;
-  while (get_size(get_header_ptr(ptr)) > 0) {
-    printf("%d. ", n);
-    print_block(ptr);
-
-    ptr = (unsigned int*) get_next_block_ptr(ptr);
-    n++;
-  }
-  printf("E. ");
-  print_block(ptr);
-
-  printf("--------------- END HEAP DUMP ---------------\n\n");
-}
-
-static void *coalesce(void *bp) {
-	size_t prev_alloc = is_allocated(get_footer_ptr(get_prev_block_ptr(bp)));
-	size_t next_alloc = is_allocated(get_header_ptr(get_next_block_ptr(bp)));
-	size_t size = get_size(get_header_ptr(bp));
-
-	if (prev_alloc && next_alloc) {
-		return bp;
-	}
-  else if (prev_alloc && !next_alloc) {
-		size += get_size(get_header_ptr(get_next_block_ptr(bp)));
-		set_val(get_header_ptr(bp), make_header(size, 0));
-		set_val (get_footer_ptr(bp), make_header(size,0));
-	}
-  else if (!prev_alloc && next_alloc) {
-		size += get_size(get_header_ptr(get_prev_block_ptr(bp)));
-		set_val(get_footer_ptr(bp), make_header(size, 0));
-		set_val(get_header_ptr(get_prev_block_ptr(bp)), make_header(size, 0));
-		bp = get_prev_block_ptr(bp);
-	}
-  else {
-		size += get_size(get_header_ptr(get_prev_block_ptr(bp))) +
-			get_size(get_footer_ptr(get_next_block_ptr(bp)));
-		set_val(get_header_ptr(get_prev_block_ptr(bp)), make_header(size, 0));
-		set_val(get_footer_ptr(get_next_block_ptr(bp)), make_header(size, 0));
-		bp = get_prev_block_ptr(bp);
-	}
-	return bp;
-}
-
-static void *extend_heap(size_t words) {
-  char *bp;
-  size_t size;
-
-  /* Allocate an even number of words to maintain alignment */
-  size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
-  if ((long)(bp = mem_sbrk(size)) == -1)
-    return NULL;
-
-  /* Initialize free block header/footer and the epilogue header */
-  set_val(get_header_ptr(bp), make_header(size, 0)); /* Free block header */
-  set_val(get_footer_ptr(bp), make_header(size, 0)); /* Free block footer */
-  set_val(get_header_ptr(get_next_block_ptr(bp)), make_header(0, 1)); /* New epilogue header */
-
-  /* Coalesce if the previous block was free */
-  return coalesce(bp);
-}
+/* Global (private) variables */
+static void *heap_ptr = NULL;
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-  heap_listp = mem_sbrk(4 * WSIZE);
+  heap_ptr = mem_sbrk(4 * WSIZE);
 
-  if (heap_listp == (void *) -1) {
+  if (heap_ptr == (void *) -1) {
     return -1;
   }
 
-  set_val(heap_listp, 0);                            /* Alignment padding */
-  set_val(heap_listp + (1 * WSIZE), make_header(DSIZE, 1)); /* Prologue header */
-  set_val(heap_listp + (2 * WSIZE), make_header(DSIZE, 1)); /* Prologue footer */
-  set_val(heap_listp + (3 * WSIZE), make_header(0, 1));     /* Epilogue header */
+  set_val(heap_ptr, 0);                            /* Alignment padding */
+  set_val(heap_ptr + (1 * WSIZE), make_header(DSIZE, 1)); /* Prologue header */
+  set_val(heap_ptr + (2 * WSIZE), make_header(DSIZE, 1)); /* Prologue footer */
+  set_val(heap_ptr + (3 * WSIZE), make_header(0, 1));     /* Epilogue header */
 
-  heap_listp += (2 * WSIZE); // Start heap at prologue footer
+  heap_ptr += (2 * WSIZE); // Start heap at prologue footer
 
   /* Extend the empty heap with a free block of CHUNKSIZE bytes */
   if (extend_heap(CHUNKSIZE / WSIZE) == NULL) {
@@ -185,40 +104,6 @@ int mm_init(void)
   return 0;
 }
 
-/* Searches the heap for a block large enough to hold `size` using first-fit. */
-static void *find_fit(size_t size) {
-  void *ptr = heap_listp;
-
-  // All blocks are at least 1 in size except the epilogue (end of heap)
-  while (get_size(get_header_ptr(ptr)) > 0) {
-    if (is_free(get_header_ptr(ptr)) && get_size(get_header_ptr(ptr)) >= size) {
-      return ptr;
-    }
-
-    ptr = get_next_block_ptr(ptr);
-  }
-
-  return NULL;
-}
-
-static void place(void *ptr, size_t size) {
-  size_t cur_size = get_size(get_header_ptr(ptr));
-  size_t remaining_size = cur_size - size;
-
-  // There is space for another block, so we split
-  if (remaining_size >= MIN_BLOCK_SIZE) {
-    set_val(get_header_ptr(ptr), make_header(size, 1));
-    set_val(get_footer_ptr(ptr), make_header(size, 1));
-
-    set_val(get_header_ptr(get_next_block_ptr(ptr)), make_header(remaining_size, 0));
-    set_val(get_footer_ptr(get_next_block_ptr(ptr)), make_header(remaining_size, 0));
-  }
-  // There is not space for another block, so we don't split
-  else {
-    set_val(get_header_ptr(ptr), make_header(cur_size, 1));
-    set_val(get_footer_ptr(ptr), make_header(cur_size, 1));
-  }
-}
 
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
@@ -288,9 +173,123 @@ void *mm_realloc(void *ptr, size_t size)
   return newptr;
 }
 
-//void mm_check() {
-//  printf("- Heap boundary: %p to %p\n", mem_heap_lo(), mem_heap_hi());
-//  printf("- Heap size: %d\n", mem_heapsize());
-//  printf("- Heap start ptr: %p\n", heap_listp);
-//}
+/* Searches the heap for a block large enough to hold `size` using first-fit. */
+static void *find_fit(size_t size) {
+  void *ptr = heap_ptr;
 
+  // All blocks are at least 1 in size except the epilogue (end of heap)
+  while (get_size(get_header_ptr(ptr)) > 0) {
+    if (is_free(get_header_ptr(ptr)) && get_size(get_header_ptr(ptr)) >= size) {
+      return ptr;
+    }
+
+    ptr = get_next_block_ptr(ptr);
+  }
+
+  return NULL;
+}
+
+/*
+ * Places a block at the given block pointer. If the placed block leaves space
+ * for another block after it, the existing block will be split in two.
+ */
+static void place(void *ptr, size_t size) {
+  size_t cur_size = get_size(get_header_ptr(ptr));
+  size_t remaining_size = cur_size - size;
+
+  // There is space for another block, so we split
+  if (remaining_size >= MIN_BLOCK_SIZE) {
+    set_val(get_header_ptr(ptr), make_header(size, 1));
+    set_val(get_footer_ptr(ptr), make_header(size, 1));
+
+    set_val(get_header_ptr(get_next_block_ptr(ptr)), make_header(remaining_size, 0));
+    set_val(get_footer_ptr(get_next_block_ptr(ptr)), make_header(remaining_size, 0));
+  }
+  // There is not space for another block, so we don't split
+  else {
+    set_val(get_header_ptr(ptr), make_header(cur_size, 1));
+    set_val(get_footer_ptr(ptr), make_header(cur_size, 1));
+  }
+}
+
+/* Increases the size of the heap with the given number of words */
+static void *extend_heap(size_t words) {
+  char *bp;
+  size_t size;
+
+  /* Allocate an even number of words to maintain alignment */
+  size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+  if ((long)(bp = mem_sbrk(size)) == -1)
+    return NULL;
+
+  /* Initialize free block header/footer and the epilogue header */
+  set_val(get_header_ptr(bp), make_header(size, 0)); /* Free block header */
+  set_val(get_footer_ptr(bp), make_header(size, 0)); /* Free block footer */
+  set_val(get_header_ptr(get_next_block_ptr(bp)), make_header(0, 1)); /* New epilogue header */
+
+  /* Coalesce if the previous block was free */
+  return coalesce(bp);
+}
+
+static void *coalesce(void *bp) {
+	size_t prev_alloc = is_allocated(get_footer_ptr(get_prev_block_ptr(bp)));
+	size_t next_alloc = is_allocated(get_header_ptr(get_next_block_ptr(bp)));
+	size_t size = get_size(get_header_ptr(bp));
+
+	if (prev_alloc && next_alloc) {
+		return bp;
+	}
+  else if (prev_alloc && !next_alloc) {
+		size += get_size(get_header_ptr(get_next_block_ptr(bp)));
+		set_val(get_header_ptr(bp), make_header(size, 0));
+		set_val (get_footer_ptr(bp), make_header(size,0));
+	}
+  else if (!prev_alloc && next_alloc) {
+		size += get_size(get_header_ptr(get_prev_block_ptr(bp)));
+		set_val(get_footer_ptr(bp), make_header(size, 0));
+		set_val(get_header_ptr(get_prev_block_ptr(bp)), make_header(size, 0));
+		bp = get_prev_block_ptr(bp);
+	}
+  else {
+		size += get_size(get_header_ptr(get_prev_block_ptr(bp))) +
+			get_size(get_footer_ptr(get_next_block_ptr(bp)));
+		set_val(get_header_ptr(get_prev_block_ptr(bp)), make_header(size, 0));
+		set_val(get_footer_ptr(get_next_block_ptr(bp)), make_header(size, 0));
+		bp = get_prev_block_ptr(bp);
+	}
+	return bp;
+}
+
+/* Debugging utilities */
+static void print_block(void *ptr) {
+    unsigned int* next = (unsigned int*) get_next_block_ptr(ptr);
+    printf("[%s] Addr: %p. Size: %d. Next: %p.\n",
+      is_allocated(get_header_ptr(ptr)) ? "Allocated" : "Free     ",
+      ptr, get_size(get_header_ptr(ptr)), next);
+}
+
+static void heapdump() {
+  printf("\n");
+  printf("----------------- HEAP DUMP -----------------\n");
+
+  printf("- Heap boundary: %p to %p\n", mem_heap_lo(), mem_heap_hi());
+  printf("- Heap size: %d bytes\n", mem_heapsize());
+  printf("- Heap start ptr: %p\n", heap_ptr);
+
+  printf("\nHeap dump:\n");
+
+  // Print all blocks
+  int n = 0;
+  unsigned int* ptr = (unsigned int*) heap_ptr;
+  while (get_size(get_header_ptr(ptr)) > 0) {
+    printf("%d. ", n);
+    print_block(ptr);
+
+    ptr = (unsigned int*) get_next_block_ptr(ptr);
+    n++;
+  }
+  printf("E. ");
+  print_block(ptr);
+
+  printf("--------------- END HEAP DUMP ---------------\n\n");
+}
